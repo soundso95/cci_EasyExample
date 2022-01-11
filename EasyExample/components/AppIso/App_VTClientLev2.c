2238b3361c9a79ab4b95af97c639ef73cd544d59
Andreas Vogler <l1622@schueler.ms-muc.de>
2022-01-11 09:44:33 +0100

    Lösungsvariante 1 mit if

--- EasyExample/components/AppIso/App_VTClientLev2.c
+++ EasyExample/components/AppIso/App_VTClientLev2.c
@@ -173,6 +173,15 @@ void VTC_handleSoftkeysAndButtons_RELEASED(const struct ButtonActivation_S *pBut
 	default:
 		break;
 	}
+	if(Gesamtzaehler ==1)
+		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 1, 440, 100, 200);
+	if(Gesamtzaehler ==2)
+		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 2, 440, 100, 200);
+	if(Gesamtzaehler ==3)
+		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 3, 440, 100, 200);
+
+
+
 	// Senden des Wertes der lokalen Variable Tageszaehler an die NumberVariable_Tageszaehler
 	IsoVtcCmd_NumericValue(pButtonData->u8Instance, NumberVariable_Tageszaehler, Tageszaehler);
 	// Senden des Wertes der lokalen Variable Gesamtzaehler an die NumberVariable_Gesamtzaehler
